8987362ef1368d8b31c83d084cf592b8d64a678e
fa-bs <0fabiobs@gmail.com>
2023-09-07 00:58:33 -0300

    add test site

---
+++ docs/_nuxt/index.a7a1139f.js
@@ -0,0 +1,1 @@
+import{r as u,y as c,k as f,z as l,A as p}from"./entry.2988a368.js";function d(t,a){const e=u({});return c(()=>{const s=l(t),{title:n,titleTemplate:o,...r}=s;e.value={title:n,titleTemplate:o,meta:p(r)}}),f(e,a)}export{d as u};
---
+++ docs/_nuxt/useState.deb90592.js
@@ -0,0 +1,1 @@
+import{g as o}from"./entry.2988a368.js";const s=()=>o("counterComposable",()=>0),r=()=>o("color",()=>"orange");export{s as a,r as u};
