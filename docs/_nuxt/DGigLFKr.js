7747fb16aac4cba75f06b484e50071df6c058313
fa-bs <0.fabiobs@gmail.com>
2025-04-22 22:10:23 -0300

    deploy-site-42

---
+++ docs/_nuxt/DGigLFKr.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./Dw6Fx_4s.js";import{_ as s,c as n,o as r,a as c,d as e,ao as _,F as l}from"./DTDEcihn.js";import"./DNAfLHxs.js";import"./BVB9nWMK.js";const m={},d={class:"w-[100vw]"};function i(o,p){const t=a;return r(),n(l,null,[c(t),e("div",null,[e("main",d,[_(o.$slots,"default")])])],64)}const B=s(m,[["render",i]]);export{B as default};
