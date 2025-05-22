8e99c1a0741dc8a2b3c5e0206dd3e94f72fb2343
fa-bs <0.fabiobs@gmail.com>
2025-05-22 01:20:48 -0300

    deploy-site-56

--- docs/_nuxt/CTyOe0Wg.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./CS_JKuj3.js";import{_ as s,c as n,o as r,a as c,d as e,ao as _,F as l}from"./Cr_kF6iI.js";import"./BDCuNM0d.js";import"./TRgaxYMm.js";const m={},d={class:"w-[100vw]"};function i(o,p){const t=a;return r(),n(l,null,[c(t),e("div",null,[e("main",d,[_(o.$slots,"default")])])],64)}const B=s(m,[["render",i]]);export{B as default};
