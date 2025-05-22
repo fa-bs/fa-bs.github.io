6c7343ac8c1e68bf22b6144421def9b575ac19c9
fa-bs <0.fabiobs@gmail.com>
2025-05-22 01:07:54 -0300

    deploy-site-55

--- docs/_nuxt/BP3FYC1a.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./Bt5NdTNF.js";import{_ as s,c as n,o as r,a as c,d as e,ao as _,F as l}from"./ShpUZv6D.js";import"./CNZkP_Ul.js";import"./VsE-wRTe.js";const m={},d={class:"w-[100vw]"};function i(o,p){const t=a;return r(),n(l,null,[c(t),e("div",null,[e("main",d,[_(o.$slots,"default")])])],64)}const B=s(m,[["render",i]]);export{B as default};
