80fec4f781c4210625916b77a09804bca2f2302e
fa-bs <0.fabiobs@gmail.com>
2025-05-29 01:49:33 -0300

    deploy-site-66

---
+++ docs/_nuxt/BYmRNNch.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./BalkytAo.js";import{_ as a,c as n,o as r,a as c,d as e,ao as _,F as l}from"./B1jaBGRu.js";import"./B94-O13h.js";import"./DL1WE3HV.js";const m={},i={class:"flex justify-center"};function d(o,f){const t=s;return r(),n(l,null,[c(t),e("div",null,[e("main",i,[_(o.$slots,"default")])])],64)}const N=a(m,[["render",d]]);export{N as default};
