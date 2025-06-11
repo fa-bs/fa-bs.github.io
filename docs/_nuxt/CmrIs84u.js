c52cb0d8a357f7d503f31a1dbb09e406f658ba6a
fa-bs <0.fabiobs@gmail.com>
2025-06-11 01:02:38 -0300

    deploy-site-72

---
+++ docs/_nuxt/CmrIs84u.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./Bee38S5f.js";import{_ as a,c as n,o as r,a as c,d as e,an as _,F as l}from"./CU28usIv.js";import"./DC1qXNGD.js";import"./C3r2QBDE.js";const m={},i={class:"flex justify-center"};function d(o,f){const t=s;return r(),n(l,null,[c(t),e("div",null,[e("main",i,[_(o.$slots,"default")])])],64)}const N=a(m,[["render",d]]);export{N as default};
