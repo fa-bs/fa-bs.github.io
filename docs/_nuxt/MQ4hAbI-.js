7427e7111b5ed4a706a0b7aa5af404456d6efd3c
fa-bs <0.fabiobs@gmail.com>
2025-07-17 20:43:11 -0300

    deploy-site-77

--- docs/_nuxt/MQ4hAbI-.js
+++
@@ -1,1 +0,0 @@
-import{_ as s}from"./BNdBf5Uh.js";import{_ as a,c as n,o as r,a as c,d as e,an as _,F as l}from"./BjVGRr6Z.js";import"./BtZ_QgUD.js";import"./DsY0qfm0.js";const m={},i={class:"flex justify-center"};function d(o,f){const t=s;return r(),n(l,null,[c(t),e("div",null,[e("main",i,[_(o.$slots,"default")])])],64)}const N=a(m,[["render",d]]);export{N as default};
